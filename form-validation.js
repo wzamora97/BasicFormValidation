7f20fbab276b7c751613dcd6945a8293be0d08a3
William Zamora <wzamora@mail.csuchico.edu>
2017-03-07 10:43:57 -0800

    checked if password entered was password

--- form-validation.js
+++ form-validation.js
@@ -14,10 +14,16 @@ function validate(){
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
 }
+  if (passEntered == "password") {
+
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
+else {
+  document.getElementById("passwordGroup").classList.add("has-success");
+}
 }
