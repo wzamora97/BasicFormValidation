f89eca3a3e68b15f76edd0e17ed696ecf3c93784
William Zamora <wzamora@mail.csuchico.edu>
2017-03-07 10:31:41 -0800

    Password will turn green if its greater than 6

--- form-validation.js
+++ form-validation.js
@@ -2,14 +2,18 @@ function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
+  var size = userEntered.length;
+  if (size >= 6) {
+      document.getElementById("usernameGroup").classList.add("has-success");
+  }
 
-  //Show message that there is an error with the username...
+  else{//Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
-
+}
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
